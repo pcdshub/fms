bbc2af30d41b6a2801ed18722a55938d06e5c0c3
Nick Waters <nrw@psbuild-rhel7-01.slac.stanford.edu>
2024-10-16 15:39:40 -0700

    TST: testing containers

--- fms/tests/test_containers.py
+++ fms/tests/test_containers.py
@@ -1,6 +1,6 @@
-from .containers import FMSRaritanItem, FMSBeckhoffItem, FMSSRCItem, FMSItem
+from ..happi.containers import FMSRaritanItem, FMSBeckhoffItem, FMSSRCItem, FMSItem
 from happi import Client
-from happi.errors import DuplicateError
+from happi.errors import DuplicateError, EntryError
 from happi.item import OphydItem
 from happi.backends.json_db import JSONBackend
 import os, pytest
@@ -58,9 +58,17 @@ def test_FMSItem(clean_files):
 
     item.save()
     assert_valid(client)
+
+def test_FMSRaritanItem(clean_files):
+    client = Client(path=test_file) 
     
+    with pytest.raises(EntryError):
+        item = client.create_item(item_cls=FMSRaritanItem,
+                name="test",
+                prefix="TEST:PV",
+                high_alarm=1,
+                moderate_alarm=1,
+                low_alarm=1,
+                bottom_alarm=1)
 
-    
-
-
- 
+        item.save()
