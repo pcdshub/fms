59f127b191af94b643a6cc94a5f0cbd866c29206
Nick Waters <nrw@psbuild-rhel7-01.slac.stanford.edu>
2024-10-24 16:44:46 -0700

    TST: add more tests

--- fms/tests/test_fms.py
+++ fms/tests/test_fms.py
@@ -4,6 +4,7 @@ from happi.item import OphydItem
 from happi.backends.json_db import JSONBackend
 import os, pytest, json
 from .test_utils import assert_invalid, assert_valid
+from fms.__main__ import add_src_controller, add_fms_sensor
 
 test_file = "test.db"
 
@@ -11,11 +12,14 @@ test_file = "test.db"
 def clean_files():
     db = JSONBackend(path=test_file, initialize=True)
     client = Client(path=test_file) 
-    sensor = [("temp1", "3")]
+    sensor = [("temp1", "3"), ("temp2", "4")]
+
     yield client, sensor
 
+    assert_valid(client)
     if os.path.exists(test_file):
         os.remove(test_file)
+
 
 def test_add(clean_files):
     client, sensor = clean_files
@@ -26,7 +30,10 @@ def test_add(clean_files):
     assert_valid(client)
 
 def test_add_src(clean_files):
+    sensor_name = 0
+    distance = 1
     client, sensor = clean_files
+    assert(sensor[0][sensor_name] == "temp1") 
     sensor_to_add = json.dumps(sensor)
 
     item = client.create_item(item_cls=FMSSRCItem,
@@ -36,3 +43,34 @@ def test_add_src(clean_files):
 
     item.save()
     assert_valid(client)
+
+    item = client.find_item(name="test_src")
+    child_list = json.loads(item.port0)
+    assert(sensor[0][sensor_name] == "temp1") 
+    assert(sensor[1][sensor_name] == "temp2") 
+
+def test_add_src_controller(clean_files, monkeypatch):
+    monkeypatch.setattr('builtins.input', lambda _: "a_new_src_controller")
+    client, sensor = clean_files
+    add_src_controller(client)
+
+    item = client.find_item(name="a_new_src_controller")
+    assert(item.name == "a_new_src_controller")
+
+    monkeypatch.setattr('builtins.input', lambda _: 1)
+    with pytest.raises(ValueError):
+        add_src_controller(client)
+
+def test_add_raritan_sensor(clean_files, monkeypatch):
+    inputs = iter(["foo", "Raritan","src-mec-01","3", 10, ])
+    client, sensor = clean_files
+
+    monkeypatch.setattr('builtins.input', lambda _:next(inputs))
+    add_fms_sensor(client)
+
+    #inputs = iter(["foo_name", "foo_type", "foo_type", "foo_type","src-mec-01","3", 10, ])
+    #onkeypatch.setattr('builtins.input', lambda _:next(inputs))
+
+
+    #ith pytest.raises(ValueError):
+    #   add_fms_sensor(client)
